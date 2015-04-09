bcf6b38c67256138f0c250ae05c6152c3ae36bee
javakidx <javakidx@gmail.com>
2015-04-09 13:43:24 +0800

    Initial commit

---
+++ src/idv/jk/study/action/HelloWorldAction.java
@@ -0,0 +1,15 @@
+package idv.jk.study.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8636562093302953416L;
+
+	public String execute() throws Exception{
+		return SUCCESS;
+	}
+}
